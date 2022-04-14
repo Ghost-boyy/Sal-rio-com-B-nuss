d66903a9756d248baa82007be1f75ecc8318f86a
anate <anate@JHON>
2022-04-14 13:41:35 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Salário_com_Bônus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome;
+            double salarioFixo, totalVenda, salarioTotal;
+
+            nome = Console.ReadLine();
+            salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            totalVenda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salarioTotal = totalVenda * 15.0 / 100 + salarioFixo;
+
+            Console.WriteLine("TOTAL = R$ " + salarioTotal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
